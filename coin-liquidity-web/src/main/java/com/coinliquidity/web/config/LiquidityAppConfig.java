ec4f9e70370720f82abba407007fbb510c3288f7
coin-liquidity <coin_liquidity@tuta.io>
2016-09-28 19:01:39 -0400

    add profile

--- coin-liquidity-web/src/main/java/com/coinliquidity/web/config/LiquidityAppConfig.java
+++ coin-liquidity-web/src/main/java/com/coinliquidity/web/config/LiquidityAppConfig.java
@@ -5,8 +5,10 @@ import com.coinliquidity.web.LiquidityCache;
 import com.coinliquidity.web.rest.LiquidityController;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("production")
 public class LiquidityAppConfig {
 
     @Bean
