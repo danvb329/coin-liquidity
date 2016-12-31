26bbdb7b930830092cd7ff29b3647f1162379bdf
coin-liquidity <coin_liquidity@tuta.io>
2016-12-31 11:27:54 -0500

    change order of commands

--- config/update.sh
+++ config/update.sh
@@ -1,8 +1,9 @@
-systemctl stop coin-liquidity.service
-rm coin-liquidity.jar
 cd coin-liquidity
 git pull
 mvn clean package
 cd ..
+
+systemctl stop coin-liquidity.service
+rm coin-liquidity.jar
 cp coin-liquidity/coin-liquidity-web/target/coin-liquidity-web-0.1-SNAPSHOT.jar coin-liquidity.jar
 systemctl start coin-liquidity.service
