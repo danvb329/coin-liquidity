b6887ba541a2ec95040676d57380b7d1acf34063
coin-liquidity <coin_liquidity@tuta.io>
2016-12-31 11:16:01 -0500

    added stop/start commands

--- config/update.sh
+++ config/update.sh
@@ -1,6 +1,8 @@
+systemctl stop coin-liquidity.service
 rm coin-liquidity.jar
 cd coin-liquidity
 git pull
 mvn clean package
 cd ..
 cp coin-liquidity/coin-liquidity-web/target/coin-liquidity-web-0.1-SNAPSHOT.jar coin-liquidity.jar
+systemctl start coin-liquidity.service
