1a394d8cc7afdc1ce50cd6af96498139f2723afc
coin-liquidity <coin_liquidity@tuta.io>
2017-03-05 11:24:58 -0500

    load historical file data

--- coin-liquidity-web/src/main/java/com/coinliquidity/web/config/LiquidityAppConfig.java
+++ coin-liquidity-web/src/main/java/com/coinliquidity/web/config/LiquidityAppConfig.java
@@ -2,9 +2,7 @@ package com.coinliquidity.web.config;
 
 import com.coinliquidity.core.ExchangeConfig;
 import com.coinliquidity.web.LiquidityCache;
-import com.coinliquidity.web.model.LiquidityData;
 import com.coinliquidity.web.persist.DbPersister;
-import com.coinliquidity.web.persist.FilePersister;
 import com.coinliquidity.web.persist.LiquidityDataPersister;
 import com.coinliquidity.web.rest.DataController;
 import com.coinliquidity.web.rest.LiquidityController;
@@ -13,11 +11,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import java.time.Instant;
-import java.util.List;
-
-import static java.time.temporal.ChronoUnit.DAYS;
 
 @Configuration
 @Profile("production")
@@ -46,17 +39,17 @@ public class LiquidityAppConfig {
 
     @Bean
     public LiquidityDataPersister liquidityDataPersister(final JdbcTemplate jdbcTemplate) {
-        final String dataDir = System.getProperty("liquidity.data.dir");
-        if (dataDir == null) {
-            throw new RuntimeException("Must specify liquidity.data.dir system property!");
-        }
-        final FilePersister filePersister = new FilePersister(dataDir);
-        List<LiquidityData> fileHistory = filePersister.loadHistory(Instant.now().minus(60, DAYS));
-
-        final DbPersister dbPersister = new DbPersister(jdbcTemplate);
-        fileHistory.forEach(dbPersister::persist);
-        return dbPersister;
-        //return new DbPersister(jdbcTemplate);
+//        final String dataDir = System.getProperty("liquidity.data.dir");
+//        if (dataDir == null) {
+//            throw new RuntimeException("Must specify liquidity.data.dir system property!");
+//        }
+//        final FilePersister filePersister = new FilePersister(dataDir);
+//        List<LiquidityData> fileHistory = filePersister.loadHistory(Instant.now().minus(60, DAYS));
+//
+//        final DbPersister dbPersister = new DbPersister(jdbcTemplate);
+//        fileHistory.forEach(dbPersister::persist);
+//        return dbPersister;
+        return new DbPersister(jdbcTemplate);
     }
 
     @Bean
