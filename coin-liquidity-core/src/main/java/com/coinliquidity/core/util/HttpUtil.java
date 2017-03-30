26a298ef5bde33e55f4726d73024159034e4e6ce
coin-liquidity <coin_liquidity@tuta.io>
2017-03-30 07:07:36 -0400

    fix gzip content encoding (used by okcoin.cn)

--- coin-liquidity-core/src/main/java/com/coinliquidity/core/util/HttpUtil.java
+++ coin-liquidity-core/src/main/java/com/coinliquidity/core/util/HttpUtil.java
@@ -10,8 +10,11 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.zip.GZIPInputStream;
 
 public class HttpUtil {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     public static JsonNode get(final String url) {
         final Client client = ClientBuilder.newClient();
@@ -21,9 +24,23 @@ public class HttpUtil {
         final WebTarget target = client.target(url);
         final Response response = target.request().get();
         try {
-            return new ObjectMapper().readTree(response.readEntity(InputStream.class));
+            final InputStream is = response.readEntity(InputStream.class);
+            return readTree(is, isGzip(response));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static boolean isGzip(final Response response) {
+        final String contentEncoding = response.getHeaderString("Content-Encoding");
+        return contentEncoding != null && contentEncoding.equals("gzip");
+    }
+
+    private static JsonNode readTree(final InputStream is, boolean gzip) throws IOException {
+        if (gzip) {
+            return MAPPER.readTree(new GZIPInputStream(is));
+        } else {
+            return MAPPER.readTree(is);
         }
     }
 }
