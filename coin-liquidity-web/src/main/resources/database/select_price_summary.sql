91be3e116c68bb9d4d71ca1586f62f04eda46745
coin-liquidity <coin_liquidity@tuta.io>
2017-04-03 19:03:18 -0400

    bitstamp.com -> bitstamp.net

--- coin-liquidity-web/src/main/resources/database/select_price_summary.sql
+++ coin-liquidity-web/src/main/resources/database/select_price_summary.sql
@@ -3,5 +3,5 @@ SELECT
     (AVG(best_bid) + AVG(best_ask)) / 2 AS avg_price
 FROM liquidity_history
 WHERE base_ccy = ? AND run_date > ?
-AND exchange IN ('gdax.com', 'gemini.com', 'poloniex.com', 'bitfinex.com', 'bitstamp.com')
+AND exchange IN ('gdax.com', 'gemini.com', 'poloniex.com', 'bitfinex.com', 'bitstamp.net')
 GROUP BY run_date
