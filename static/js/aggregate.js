7a544d4d8d08c6d18d3b27064e88dd3029eabb78
coin-liquidity <coin_liquidity@tuta.io>
2018-03-07 17:22:50 -0500

    dependency updates

---
+++ static/js/aggregate.js
@@ -0,0 +1,25 @@
+$.tablesorter.addParser({
+    id: 'thousands',
+    is: function(s) {
+        return false;
+    },
+    format: function(s) {
+        if (s === 'n/a') return "99999999999999";
+        return s.replace('$','').replace(/,/g,'');
+    },
+    type: 'numeric'
+});
+
+$(function() {
+    $("table").tablesorter({
+        headers: {
+            1: { sorter:'thousands' },
+            2: { sorter:'thousands' },
+            3: { sorter:'thousands' },
+            4: { sorter:'thousands' },
+            5: { sorter:'thousands' },
+            6: { sorter:'thousands' }
+        },
+        sortList: [[1,1]]
+    });
+});
